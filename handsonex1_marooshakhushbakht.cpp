194caa55217841a5f2926f99039c670862b846d0
maroosha-khushbakht <f2025-0721@bnu.edu.pk>
2025-10-03 10:52:25 +0500

    Add files via upload

---
+++ handsonex1_marooshakhushbakht.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    int x, y;
+    cout << "Enter Number 1: ";
+    cin >> x;
+    cout << "Enter Number 2: ";
+    cin >> y;
+    
+    int sum = x + y;
+    cout << "Sum: " << sum << endl;
+    
+    if (x>y) 
+    cout << "Difference: " << x-y << endl;
+    else
+    cout << "Difference: " << y-x << endl;
+    
+    int p = x*y;
+    cout << "Product: " << p << endl;
+    
+     if (x>y) 
+    cout << "Quotient: " << x/y << endl;
+    else
+    cout << "Quotient: " << y/x << endl;
+    
+   
+
+    return 0;
+}
